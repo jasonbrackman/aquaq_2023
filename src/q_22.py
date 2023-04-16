33cd3a74d0115bd56d550cd87eeaef67b0099b88
jasonbrackman <brackman@gmail.com>
2023-04-16 10:21:43 -0700

    Blacked solution 22

--- src/q_22.py
+++ src/q_22.py
@@ -1,25 +1,26 @@
 import os
 from string import ascii_uppercase
 
+
 def to_roman(num: int) -> str:
     # Define the Roman numeral symbols and their corresponding values
     roman_numerals = {
-        1000: 'M',
-        900: 'CM',
-        500: 'D',
-        400: 'CD',
-        100: 'C',
-        90: 'XC',
-        50: 'L',
-        40: 'XL',
-        10: 'X',
-        9: 'IX',
-        5: 'V',
-        4: 'IV',
-        1: 'I'
+        1000: "M",
+        900: "CM",
+        500: "D",
+        400: "CD",
+        100: "C",
+        90: "XC",
+        50: "L",
+        40: "XL",
+        10: "X",
+        9: "IX",
+        5: "V",
+        4: "IV",
+        1: "I",
     }
     # Initialize an empty string to store the Roman numeral representation
-    roman_numeral = ''
+    roman_numeral = ""
     # Iterate over the symbols in descending order of their values
     for value, symbol in roman_numerals.items():
         # Repeat the symbol as many times as possible
@@ -35,7 +36,7 @@ def run() -> None:
     total = 0
     # Don't understand the puzzle joke of changing the `vici` to `vitavi`
     #                                               `Conquer` vs `Shun`
-    with open(r'./data/22_veni_vidi_vitavi.txt', 'r') as f:
+    with open(r"./data/22_veni_vidi_vitavi.txt", "r") as f:
         for item in f.read().split():
             result = to_roman(int(item))
             for r in result:
